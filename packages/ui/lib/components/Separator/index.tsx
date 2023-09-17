c60394db954c5d16c429f158d8f45355a76a2c85
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-09-17 17:47:17 +0300

    add sheet component

---
+++ packages/ui/lib/components/Separator/index.tsx
@@ -0,0 +1,29 @@
+import { cn } from "../../utils";
+
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import * as React from "react";
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref,
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "shrink-0 bg-gray-900",
+        orientation === "horizontal" ? "h-[2px] w-full" : "h-full w-[2px]",
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
+
+export { Separator };
