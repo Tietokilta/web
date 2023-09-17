b81bb27b37da7317185b9f713c787617f06603b8
jolle <jolle@users.noreply.github.com>
2023-09-17 16:04:28 +0300

    Add footer global

---
+++ packages/cms/src/globals/Footer.ts
@@ -0,0 +1,103 @@
+import { iconField } from "../fields/iconField";
+import { LinkRowBlockLink } from "../payload-types";
+
+import { Block, GlobalConfig } from "payload/types";
+
+const LinkRowBlock: Block = {
+  slug: "link-row",
+  fields: [
+    {
+      name: "showLabel",
+      type: "checkbox",
+      defaultValue: true,
+      required: true,
+    },
+    {
+      name: "links",
+      type: "array",
+      interfaceName: "LinkRowBlockLink",
+      fields: [
+        iconField({ required: true }),
+        {
+          name: "label",
+          type: "text",
+          required: true, // always required (used in alt text when showLabel = false)
+          localized: true,
+        },
+        {
+          name: "linkType",
+          type: "radio",
+          options: [
+            {
+              label: "External URL",
+              value: "external",
+            },
+            {
+              label: "Internal page",
+              value: "internal",
+            },
+          ],
+        },
+        {
+          name: "url",
+          type: "text",
+          required: true,
+          admin: {
+            condition: (_, siblingData: Partial<LinkRowBlockLink[number]>) =>
+              siblingData.linkType === "external",
+          },
+        },
+        {
+          name: "page",
+          type: "relationship",
+          relationTo: "pages",
+          required: true,
+          admin: {
+            condition: (_, siblingData: Partial<LinkRowBlockLink[number]>) =>
+              siblingData.linkType === "internal",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const LogoRowBlock: Block = {
+  slug: "logo-row",
+  fields: [
+    {
+      name: "logos",
+      type: "array",
+      fields: [
+        {
+          name: "image",
+          type: "relationship",
+          relationTo: "media",
+          required: true,
+        },
+        {
+          name: "link",
+          type: "text",
+        },
+      ],
+    },
+  ],
+};
+
+const Footer: GlobalConfig = {
+  slug: "footer",
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: "layout",
+      type: "blocks",
+      required: true,
+      minRows: 1,
+      blocks: [LinkRowBlock, LogoRowBlock],
+    },
+  ],
+};
+
+export default Footer;
