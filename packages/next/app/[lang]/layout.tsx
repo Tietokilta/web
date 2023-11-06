976922130e7f341c0068656f352894dcf4b829ed
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-11-06 16:55:54 +0200

    Add support for localized globals

--- packages/next/app/[lang]/layout.tsx
+++ packages/next/app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css";
 import { MainNav } from "../../components/MainNav";
 import { MobileNav } from "../../components/MobileNav";
+import "./globals.css";
 
 import { cn } from "@/ui/utils";
 import { Inter, Roboto_Mono } from "next/font/google";
@@ -10,21 +10,37 @@ import type { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Tietokilta",
-  description: "Tietokilta ry:n kotisivut",
+interface Props {
+  params: {
+    lang: "en" | "fi";
+  };
+}
+
+const localizedMetadata = {
+  fi: {
+    title: "Tietokilta",
+    description: "Tietokilta ry:n kotisivut",
+  },
+  en: {
+    title: "Computer Science Guild",
+    description: "Homepage of the Computer Science Guild",
+  },
 };
+
+export const generateMetadata = ({ params: { lang } }: Props): Metadata =>
+  localizedMetadata[lang] || localizedMetadata.fi;
 
 export default function RootLayout({
   children,
+  params: { lang },
 }: {
   children: React.ReactNode;
-}) {
+} & Props) {
   return (
-    <html lang="en">
+    <html lang={lang}>
       <body className={cn(inter.className, robotoMono.className)}>
-        <MobileNav className="sticky top-0 md:hidden" />
-        <MainNav className="sticky top-0 hidden md:block" />
+        <MobileNav locale={lang} className="sticky top-0 md:hidden" />
+        <MainNav locale={lang} className="sticky top-0 hidden md:block" />
         <div>{children}</div>
       </body>
     </html>
