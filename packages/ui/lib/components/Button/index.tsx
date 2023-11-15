a3afe0ad12a60a110a52495660a7a18f6f54b50a
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-11-15 22:35:23 +0200

    style: external link clearer hover effect

--- packages/ui/lib/components/Button/index.tsx
+++ packages/ui/lib/components/Button/index.tsx
@@ -17,13 +17,13 @@ const buttonVariants = cva(
         destructive:
           "border-2 border-gray-900 bg-danger-500 text-danger-100 hover:bg-danger-500/90 shadow-solid",
         outline:
-          "border-2 border-gray-900 hover:border-gray-800 hover:bg-gray-200/90 hover:text-gray-800",
+          "border-2 border-gray-900 hover:border-gray-800 hover:bg-gray-300/90 hover:text-gray-800",
         secondary:
           "border-2 border-gray-900 bg-gray-200 text-gray-800 hover:bg-gray-300/80 shadow-solid",
         ghost: "hover:bg-gray-100",
         link: "justify-between border-b-2 border-gray-900 rounded-none text-gray-900 after:content-['>>'] after:ml-2 hover:after:translate-x-2",
         outlineLink:
-          "border-2 border-gray-900 hover:border-gray-800 hover:bg-gray-200/90 shadow-solid",
+          "border-2 border-gray-900 hover:border-gray-800 hover:bg-gray-300/90 shadow-solid",
       },
       size: {
         default: "h-10 py-2 px-4",
