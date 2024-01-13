e2a553f0edba388c020ae9c4b1a06ac47cb81f1b
Ahlström Kalle <ahlstrom.kalle@outlook.com>
2024-01-13 14:20:34 +0200

    move autologin user to .env

--- packages/cms/src/server.ts
+++ packages/cms/src/server.ts
@@ -21,8 +21,36 @@ const start = async () => {
   await payload.init({
     secret: process.env.PAYLOAD_SECRET!,
     express: app,
-    onInit: (payload) => {
+    onInit: async (payload) => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+      if (process.env.PAYLOAD_PUBLIC_LOCAL_DEVELOPMENT === "true") {
+        const email = process.env.PAYLOAD_PUBLIC_LOCAL_DEVELOPMENT_EMAIL;
+        const password = process.env.PAYLOAD_PUBLIC_LOCAL_DEVELOPMENT_PASSWORD;
+        if (!email || !password) {
+          throw new Error(
+            "PAYLOAD_PUBLIC_LOCAL_DEVELOPMENT_EMAIL and PAYLOAD_PUBLIC_LOCAL_DEVELOPMENT_PASSWORD must be set when PAYLOAD_PUBLIC_LOCAL_DEVELOPMENT is true",
+          );
+        }
+        // check if the user exists, if not, create it
+        const user = await payload.find({
+          collection: "users",
+          where: { email: { equals: email } },
+        });
+        if (user.totalDocs === 0) {
+          payload.logger.warn(`user ${email} not found, creating...`);
+          payload.logger.warn(
+            "NOTE that it is recommended to use the seeding scripts to a get filled database for local development",
+          );
+          await payload.create({
+            collection: "users",
+            data: {
+              email,
+              password,
+            },
+          });
+          payload.logger.warn("Payload autologin enabled!");
+        }
+      }
     },
   });
 
