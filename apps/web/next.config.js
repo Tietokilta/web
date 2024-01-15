2c595727d10809b9a948e267969fe3de615022bb
Ahlström Kalle <kalle.ahlstrom@iki.fi>
2024-01-15 03:46:24 +0200

    Docker image build improvements

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,21 +1,6 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  rewrites: () => [
-    {
-      source: "/admin/:path*",
-      destination: `${process.env.PUBLIC_SERVER_URL}/admin/:path*`,
-    },
-    {
-      source: "/api/:path*",
-      destination: `${process.env.PUBLIC_SERVER_URL}/api/:path*`,
-    },
-    // TODO: only for dev:
-    {
-      source: "/media/:path*",
-      destination: `${process.env.PUBLIC_SERVER_URL}/media/:path*`,
-    },
-  ],
   images: {
     // TODO: only for dev:
     remotePatterns: [
