15f603d0ea8acafb77e3fba7e30b743a9e77ffad
Ahlström Kalle <kalle.ahlstrom@iki.fi>
2024-01-18 21:36:18 +0200

    fix revalidate page cache not work

--- apps/cms/src/preview.ts
+++ apps/cms/src/preview.ts
@@ -3,4 +3,4 @@ import type { GeneratePreviewURL } from "payload/config";
 export const generatePreviewUrl =
   <T>(getUrl: (doc: T) => string): GeneratePreviewURL =>
   (doc) =>
-    `${process.env.PUBLIC_FRONTEND_URL}/api/preview?url=${getUrl(doc as T)}`;
+    `${process.env.PUBLIC_FRONTEND_URL}/next_api/preview?url=${getUrl(doc as T)}`;
