505a7db0bb4e89f958f54cc2a99668d2be9d0d43
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2024-01-18 14:59:09 +0200

    test cdn

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,19 @@
 /** @type {import("next").NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   images: {
     // TODO: only for dev:
-    remotePatterns:
-      process.env.NODE_ENV === "development"
-        ? [
-            {
-              protocol: "http",
-              hostname: "localhost",
-              port: process.env.PAYLOAD_PORT,
-              pathname: "/media/**",
-            },
-          ]
-        : undefined,
+    remotePatterns: !isProd
+      ? [
+          {
+            protocol: "http",
+            hostname: "localhost",
+            port: process.env.PAYLOAD_PORT,
+            pathname: "/media/**",
+          },
+        ]
+      : undefined,
   },
+  assetPrefix: isProd ? "https://cdn.alpha.tietokilta.fi" : undefined,
 };
