282b59b1d9fd97e94469b8afb672965c1511e1eb
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-01-23 15:23:49 +0200

    automatically close mobile nav on clicking links

--- apps/web/src/components/mobile-nav/language-selector.tsx
+++ apps/web/src/components/mobile-nav/language-selector.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { SheetClose } from "@tietokilta/ui";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -10,20 +11,24 @@ export function LanguageSelector() {
 
   return (
     <div className="flex items-center gap-6 px-4 pt-4 text-lg font-medium">
-      <Link
-        aria-current={isFinnish ? "page" : undefined}
-        className="underline-offset-2 hover:underline aria-[current=page]:font-bold aria-[current=page]:underline"
-        href={pathname.replace(/^\/(?:en|fi)/, "/fi")}
-      >
-        FI
-      </Link>
-      <Link
-        aria-current={isEnglish ? "page" : undefined}
-        className="underline-offset-2 hover:underline aria-[current=page]:font-bold aria-[current=page]:underline"
-        href={pathname.replace(/^\/(?:en|fi)/, "/en")}
-      >
-        EN
-      </Link>
+      <SheetClose asChild>
+        <Link
+          aria-current={isFinnish ? "page" : undefined}
+          className="underline-offset-2 hover:underline aria-[current=page]:font-bold aria-[current=page]:underline"
+          href={pathname.replace(/^\/(?:en|fi)/, "/fi")}
+        >
+          FI
+        </Link>
+      </SheetClose>
+      <SheetClose asChild>
+        <Link
+          aria-current={isEnglish ? "page" : undefined}
+          className="underline-offset-2 hover:underline aria-[current=page]:font-bold aria-[current=page]:underline"
+          href={pathname.replace(/^\/(?:en|fi)/, "/en")}
+        >
+          EN
+        </Link>
+      </SheetClose>
     </div>
   );
 }
