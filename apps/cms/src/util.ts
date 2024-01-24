ad246314965ea86afe99ddfab78ae6c851c0e044
Ahlström Kalle <kalle.ahlstrom@iki.fi>
2024-01-25 01:25:27 +0200

    fix pages revalidate english-case, fix http semantics

--- apps/cms/src/util.ts
+++ apps/cms/src/util.ts
@@ -1,3 +1,5 @@
+import type { PayloadRequest } from "payload/types";
+
 export const useCloudStorage = (): boolean => {
   return (
     typeof process.env.AZURE_STORAGE_CONNECTION_STRING === "string" &&
@@ -11,3 +13,11 @@ export const useGoogleAuth = (): boolean => {
     typeof process.env.GOOGLE_OAUTH_CLIENT_SECRET === "string"
   );
 };
+export function getLocale(req: PayloadRequest): string | undefined {
+  const res =
+    typeof req.query.locale === "string" ? req.query.locale : req.locale;
+  if (!res) {
+    req.payload.logger.warn(`locale not set for ${req.path}, defaulting to fi`);
+  }
+  return res;
+}
