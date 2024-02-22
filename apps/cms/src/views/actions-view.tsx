fca56d2e1d60e111b3634a48c23ef7ba07db35bc
Joonatan Korpela <joonatan.korpela@aalto.fi>
2024-02-22 20:04:51 +0200

    feat: Committee import functionality

--- apps/cms/src/views/actions-view.tsx
+++ apps/cms/src/views/actions-view.tsx
@@ -1,34 +1,77 @@
+/* eslint-disable no-alert -- TODO: Implement proper alerting system */
 import * as React from "react";
+import { importCommittees } from "../util/import";
 
 export const ActionsView = (): React.JSX.Element => {
-  const [file, setFile] = React.useState<File | null>(null);
+  const [success, setSuccess] = React.useState<boolean>(false);
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const formData = new FormData(e.currentTarget);
+
+    const committeeYear: number = parseInt(formData.get("year") as string);
+    if (Number.isNaN(committeeYear)) {
+      alert("Invalid year");
+      return;
+    }
+
+    const file = formData.get("file");
+    if (!(file instanceof File)) {
+      alert("Invalid file");
+      return;
+    }
+    const content = await file.text();
+
+    const result = await importCommittees(content, committeeYear);
+    if (result) {
+      setSuccess(true);
     }
   };
-
-  const handleUpload = async () => {};
 
   return (
     <div style={{ margin: 20 }}>
       <a href="/admin">Back</a>
       <h1>Actions</h1>
-      <div
+      <form
         style={{
           display: "flex",
           flexDirection: "column",
           alignItems: "flex-start",
           gap: 10,
         }}
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => void handleSubmit(e)}
       >
+        <h2>Import committees</h2>
+        <label htmlFor="year">Committee year</label>
+        <input
+          id="year"
+          name="year"
+          type="number"
+          placeholder="Committee year"
+          defaultValue={new Date().getFullYear().toString()}
+        />
         <label htmlFor="file" className="sr-only">
           Choose a csv file
         </label>
-        <input id="file" type="file" onChange={handleFileChange} />
-        {file && <button onClick={void handleUpload}>Upload</button>}
-      </div>
+        <input id="file" name="file" type="file" />
+        <button type="submit">Upload</button>
+        <span
+          style={{
+            color: "red",
+            fontSize: "2em",
+            WebkitTextStroke: "1px white",
+            border: "2px solid red",
+            padding: "10px",
+          }}
+        >
+          ⚠️ DO NOT USE UNLESS YOU KNOW WHAT YOU'RE DOING! ⚠️
+        </span>
+        {success && <p style={{ color: "success" }}>Import successful!</p>}
+      </form>
     </div>
   );
 };
