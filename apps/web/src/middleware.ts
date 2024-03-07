1e87c75cf836239ed5e8d5791db3a90bcd6ebc40
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-03-07 23:02:31 +0200

    fix middleware redirects for missing locale

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,14 +1,23 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { locales } from "./lib/dictionaries";
 
 export function middleware(request: NextRequest): NextResponse {
   const pathname = request.nextUrl.pathname;
-  if (
+
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+  if (pathnameHasLocale) {
+    return NextResponse.next();
+  }
+
+  const pathnameHasCMSPath =
     pathname.startsWith("/admin") ||
     pathname.startsWith("/media") ||
     pathname.startsWith("/api") ||
-    pathname.startsWith("/oauth2")
-  ) {
+    pathname.startsWith("/oauth2");
+  if (pathnameHasCMSPath) {
     const destination = new URL(process.env.PUBLIC_SERVER_URL || "");
     const url = request.nextUrl.clone();
     url.host = destination.host;
@@ -16,13 +25,11 @@ export function middleware(request: NextRequest): NextResponse {
     url.pathname = pathname;
     return NextResponse.rewrite(url);
   }
+
   return NextResponse.redirect(
-    new URL(
-      `/fi${request.nextUrl.pathname.length === 1 ? "" : request.nextUrl.pathname}`,
-      request.url,
-    ),
+    new URL(`/fi${pathname.length === 1 ? "" : pathname}`, request.url),
   );
 }
 export const config = {
-  matcher: ["/((?!_next|fi|en|next_api).*)"],
+  matcher: ["/((?!_next|next_api).*)"],
 };
