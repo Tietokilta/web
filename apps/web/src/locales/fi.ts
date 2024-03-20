c3709b3fafb77904b56810a362f970f7686fea91
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-03-20 22:24:58 +0200

    add ilmomasiina i18n

--- apps/web/src/locales/fi.ts
+++ apps/web/src/locales/fi.ts
@@ -5,6 +5,7 @@ export default {
     "Toggle menu": "Avaa valikko",
     "Sign up": "Ilmoittaudu",
     "Read more": "Lue lisää",
+    Back: "Takaisin",
   },
   headings: {
     "Upcoming events": "Tulevat tapahtumat",
@@ -17,5 +18,29 @@ export default {
       "Virheellinen pyyntö, yritä uudelleen.",
     "ilmomasiina-unknown-error":
       "Ilmomasiinassa tapahtui virhe. Yritä myöhemmin uudelleen.",
+  },
+  ilmomasiina: {
+    Tapahtumat: "Tapahtumat",
+    Ilmoittautuneita: "Ilmoittautuneita",
+    Ilmoittautuneet: "Ilmoittautuneet",
+    Ilmoittautuminen: "Ilmoittautuminen",
+    status: {
+      "Ilmoittautuminen auki": "Ilmoittautuminen auki {endDate} asti",
+      "Ilmoittautuminen on päättynyt": "Ilmoittautuminen on päättynyt",
+      "Ilmoittautuminen alkaa": "Ilmoittautuminen alkaa {startDate}",
+      "Tapahtumaan ei voi ilmoittautua": "Tapahtumaan ei voi ilmoittautua",
+      "Ei ilmoittautuneita vielä": "Ei ilmoittautuneita vielä.",
+      Jonossa: "Jonossa {queueCount}",
+    },
+    headers: {
+      Sija: "Sija",
+      Nimi: "Nimi",
+      Kiintiö: "Kiintiö",
+      Ilmoittautumisaika: "Ilmoittautumisaika",
+      Kategoria: "Kategoria",
+      Paikka: "Paikka",
+      Alkaa: "Alkaa",
+      Loppuu: "Loppuu",
+    },
   },
 } as const;
