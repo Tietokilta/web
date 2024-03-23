8086a6b76f463b66f90dfbb5cb8e76f5c57d750a
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-03-23 13:37:02 +0200

    fix page queries only querying by slug, query by path

--- apps/web/src/lib/api/pages.ts
+++ apps/web/src/lib/api/pages.ts
@@ -2,6 +2,10 @@ import type { Page } from "@tietokilta/cms-types/payload";
 import { getOne } from "./fetcher";
 
 export const fetchPage = getOne<
-  { where: { slug: { equals: string }; topic?: { slug: { equals: string } } } },
+  {
+    where:
+      | { path: { equals: string } }
+      | { "path.fi": { equals: string } | { "path.en": { equals: string } } };
+  },
   Page
 >("/api/pages");
