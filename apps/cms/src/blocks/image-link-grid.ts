ee31632b7b3f8ad92019abe43e51d1c590860a0b
Mikael Siidorow <mikael@siidorow.com>
2024-05-18 21:03:42 +0300

    add image link grid block for partners

---
+++ apps/cms/src/blocks/image-link-grid.ts
@@ -0,0 +1,29 @@
+import type { Block } from "payload/types";
+
+export const ImageLinkGrid = {
+  slug: "image-link-grid",
+  fields: [
+    {
+      name: "images",
+      type: "array",
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+        {
+          name: "caption",
+          type: "text",
+          localized: true,
+        },
+        {
+          name: "externalLink",
+          type: "text",
+          localized: true,
+        },
+      ],
+    },
+  ],
+} satisfies Block;
