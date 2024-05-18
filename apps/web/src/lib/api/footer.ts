8fef690e79c199d4ddee96663061c6c5c2c9603c
Mikael Siidorow <mikael@siidorow.com>
2024-05-18 13:55:49 +0300

    refactor fetcher to add globalOpts and put sorting there

--- apps/web/src/lib/api/footer.ts
+++ apps/web/src/lib/api/footer.ts
@@ -2,4 +2,4 @@ import type { Footer } from "@tietokilta/cms-types/payload";
 import { getGlobal } from "./fetcher";
 
 export const fetchFooter = (locale: string) =>
-  getGlobal<Footer>("/api/globals/footer", locale);
+  getGlobal<Footer>("/api/globals/footer", { locale });
