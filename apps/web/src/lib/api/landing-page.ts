4883b4f8b21e7eff768b08d140e1734854a247da
Mikael Siidorow <mikael@siidorow.com>
2024-05-18 13:55:49 +0300

    refactor fetcher to add globalOpts and put sorting there

--- apps/web/src/lib/api/landing-page.ts
+++ apps/web/src/lib/api/landing-page.ts
@@ -2,4 +2,4 @@ import type { LandingPage } from "@tietokilta/cms-types/payload";
 import { getGlobal } from "./fetcher";
 
 export const fetchLandingPage = (locale: string) =>
-  getGlobal<LandingPage>("/api/globals/landing-page", locale);
+  getGlobal<LandingPage>("/api/globals/landing-page", { locale });
