6e673dbf7201bc01ece60a887664d368cb50ce70
Mikael Siidorow <mikael@siidorow.com>
2024-05-19 11:11:55 +0300

    add revalidation to missing collections

--- apps/cms/src/collections/weekly-newsletters/news-items.ts
+++ apps/cms/src/collections/weekly-newsletters/news-items.ts
@@ -3,6 +3,7 @@ import type { NewsItem } from "@tietokilta/cms-types/payload";
 import { signedIn } from "../../access/signed-in";
 import { newsItemCategoryField } from "../../fields/news-item-category";
 import { getLocale } from "../../util";
+import { revalidateCollection } from "../../hooks/revalidate-collection";
 
 const formatDisplayTitle: FieldHook<NewsItem> = ({ data: newsItem, req }) => {
   if (!newsItem?.title) {
@@ -86,4 +87,10 @@ export const NewsItems: CollectionConfig = {
       localized: true,
     },
   ],
+  hooks: {
+    afterChange: [
+      revalidateCollection("news-items"),
+      revalidateCollection("weekly-newsletters"),
+    ],
+  },
 };
