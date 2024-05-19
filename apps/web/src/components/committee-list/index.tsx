380329386760a62820303219eca5765357b7db2d
Mikael Siidorow <mikael@siidorow.com>
2024-05-19 12:55:10 +0300

    Revert "remove unnecesary fragments, fix key warning"
    This reverts commit 0223526fcb894aa068d71706b27134f8ca1a86ec.

--- apps/web/src/components/committee-list/index.tsx
+++ apps/web/src/components/committee-list/index.tsx
@@ -7,7 +7,7 @@ export async function CommitteeList({
   year,
 }: {
   year: CommitteesYearBlockNode["fields"]["year"];
-}): Promise<React.ReactNode> {
+}): Promise<JSX.Element | null> {
   const locale = getCurrentLocale();
   const committees = await fetchCommittees({
     where: { year: { equals: year } },
@@ -20,9 +20,13 @@ export async function CommitteeList({
     return null;
   }
 
-  return committees
-    .sort((a, b) => a.name.localeCompare(b.name, locale))
-    .map((committee) => (
-      <CommitteeCard committee={committee} key={committee.id} />
-    ));
+  return (
+    <>
+      {committees
+        .sort((a, b) => a.name.localeCompare(b.name, locale))
+        .map((committee) => (
+          <CommitteeCard committee={committee} key={committee.id} />
+        ))}
+    </>
+  );
 }
--- apps/web/src/components/magazine-list/index.tsx
+++ apps/web/src/components/magazine-list/index.tsx
@@ -41,33 +41,37 @@ export function MagazineList({
     magazine.issues.map((issue) => (issue.issue as MagazineIssue).year),
   ).sort((a, b) => Number(b) - Number(a));
 
-  return years.map((year) => (
-    <ul
-      key={year}
-      className="not-prose shadow-solid relative my-8 flex overflow-hidden rounded-md border-2 border-gray-900 px-4 pb-6 pt-16 font-mono md:-mx-8 md:px-6 lg:-mx-32 xl:-mx-48 2xl:-mx-64"
-    >
-      <div className="absolute left-0 top-0 flex w-full justify-between border-b-2 border-gray-900 bg-gray-100 p-2">
-        <div className="flex w-5 gap-1">
-          <span className="bg-secondary-600 h-2 w-2 rounded-full border border-gray-900" />
-          <span className="bg-primary-600 h-2 w-2 rounded-full border border-gray-900" />
-        </div>
-        <span className="self-center text-sm font-medium">{year}</span>
-        <div className="w-5" />
-      </div>
-      <div className="justify-left flex max-w-[800px] flex-wrap gap-8">
-        {issuesByYear[year]
-          .sort(
-            (a, b) =>
-              Number((a.issue as MagazineIssue).issueNumber) -
-              Number((b.issue as MagazineIssue).issueNumber),
-          )
-          .map(({ issue }) => (
-            <IssueCard
-              issue={issue as MagazineIssue}
-              key={(issue as MagazineIssue).id}
-            />
-          ))}
-      </div>
-    </ul>
-  ));
+  return (
+    <>
+      {years.map((year) => (
+        <ul
+          key={year}
+          className="not-prose shadow-solid relative my-8 flex overflow-hidden rounded-md border-2 border-gray-900 px-4 pb-6 pt-16 font-mono md:-mx-8 md:px-6 lg:-mx-32 xl:-mx-48 2xl:-mx-64"
+        >
+          <div className="absolute left-0 top-0 flex w-full justify-between border-b-2 border-gray-900 bg-gray-100 p-2">
+            <div className="flex w-5 gap-1">
+              <span className="bg-secondary-600 h-2 w-2 rounded-full border border-gray-900" />
+              <span className="bg-primary-600 h-2 w-2 rounded-full border border-gray-900" />
+            </div>
+            <span className="self-center text-sm font-medium">{year}</span>
+            <div className="w-5" />
+          </div>
+          <div className="justify-left flex max-w-[800px] flex-wrap gap-8">
+            {issuesByYear[year]
+              .sort(
+                (a, b) =>
+                  Number((a.issue as MagazineIssue).issueNumber) -
+                  Number((b.issue as MagazineIssue).issueNumber),
+              )
+              .map(({ issue }) => (
+                <IssueCard
+                  issue={issue as MagazineIssue}
+                  key={(issue as MagazineIssue).id}
+                />
+              ))}
+          </div>
+        </ul>
+      ))}
+    </>
+  );
 }
