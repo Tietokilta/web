f4e7dbff33f07f50f3563515be16aea53ec31663
Mikael Siidorow <mikael@siidorow.com>
2024-05-31 23:31:08 +0300

    tweak committee cards to show more members per row on each screen size

--- apps/web/src/components/committee-card/index.tsx
+++ apps/web/src/components/committee-card/index.tsx
@@ -18,7 +18,7 @@ function CommitteeMemberCard({
   const name = committeeMember.name.replace(/-/g, "\u2011"); // use non-breaking hyphens
 
   return (
-    <li className="relative flex w-full max-w-xs flex-col border-2 border-gray-900 sm:w-44">
+    <li className="relative flex flex-col border-2 border-gray-900">
       <Image
         alt={photo?.alt ?? ""}
         className="w-full border-b-2 border-gray-900 object-cover object-center"
@@ -27,7 +27,7 @@ function CommitteeMemberCard({
         width={photo?.width ? Math.trunc(photo.width) : undefined}
       />
       {isChair ? <GavelIcon className="absolute left-0 top-0 size-6" /> : null}
-      <p className="flex flex-1 flex-col flex-wrap justify-center text-balance bg-gray-100 text-center">
+      <p className="flex grow flex-col justify-center text-balance bg-gray-100 text-center">
         <span className="font-medium">{name}</span>
         <span className="text-sm">
           {insertSoftHyphens(committeeMember.title)}
@@ -47,7 +47,7 @@ export function CommitteeCard({
   return (
     <section
       className={cn(
-        "not-prose shadow-solid relative my-8 flex overflow-hidden rounded-md border-2 border-gray-900 px-4 pb-6 pt-16 font-mono md:px-6",
+        "not-prose shadow-solid relative my-8 flex overflow-hidden rounded-md border-2 border-gray-900 px-2 pb-6 pt-16 font-mono md:px-4 lg:px-6",
         !isTightLayout && "md:-mx-8 lg:-mx-32 xl:-mx-48 2xl:-mx-64",
       )}
     >
@@ -59,7 +59,7 @@ export function CommitteeCard({
         <h2 className="self-center font-medium">{committee.name}</h2>
         <div className="w-5" />
       </div>
-      <ul className="flex w-full flex-wrap justify-center gap-4 md:gap-8">
+      <ul className="grid w-full grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2 md:grid-cols-[repeat(auto-fill,minmax(120px,1fr))] md:gap-4 lg:grid-cols-[repeat(auto-fill,minmax(140px,1fr))] lg:gap-6 xl:grid-cols-[repeat(auto-fill,minmax(160px,1fr))]">
         {committee.committeeMembers.map(({ committeeMember }) => (
           <CommitteeMemberCard
             committeeMember={committeeMember as CommitteeMember}
