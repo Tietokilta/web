d6262db6db700c8f81b5bb9d0ea546db89cfdf19
Mikael Siidorow <mikael@siidorow.com>
2024-06-03 20:16:32 +0300

    chore: update deps, fix prettier-tailwind config

--- packages/ui/lib/components/sheet/index.tsx
+++ packages/ui/lib/components/sheet/index.tsx
@@ -31,7 +31,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  "data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50  gap-4 border-gray-900 bg-gray-100 transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+  "data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 gap-4 border-gray-900 bg-gray-100 transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
   {
     variants: {
       side: {
