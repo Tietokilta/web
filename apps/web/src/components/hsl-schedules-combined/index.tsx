a4042bef58544099a0dc044f0b6ee4d7d0c64140
lanttu1243 <lasse.kukkula@gmail.com>
2024-06-14 00:38:55 +0300

    Add status check to fetch

--- apps/web/src/components/hsl-schedules-combined/index.tsx
+++ apps/web/src/components/hsl-schedules-combined/index.tsx
@@ -4,11 +4,12 @@ import { RenderableStop } from "../../assets/api_hooks/hsl_api_types.ts";
 
 
 export async function HSLcombinedSchedule() {
-  var response: Response = await fetch("//localhost:3000/api/fetch-hsl-data")
-  if (response.ok) {
-    const hslData = await response.json();
-
-    console.log(hslData)
+  var response: Response = await fetch(process.env.PUBLIC_FRONTEND_URL.concat("/next_api/fetch-hsl-data"))
+  const hslData = () => {
+    if (response.status === 200) {
+      return response.json()
+  } else
+      return "Null"
   }
 
 
@@ -18,7 +19,10 @@ export async function HSLcombinedSchedule() {
         <h1 className="flex justify-center text-3xl font-bold pt-4">Aalto-yliopisto (M)</h1>
       </div>
       <div className="flex w-full p-8 pt-0 justify-between gap-4">
-        {/*{[].map(res => (<HSLSchedule key={res} className="flex flex-col gap-4" />))*/}
+        {
+          // {response.map(res => (<HSLSchedule key={res} className="flex flex-col gap-4" />))}
+          hslData().toString()
+        }
       </div>
     </div>
   )
