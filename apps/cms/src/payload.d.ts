522eb8122f96556dd9e1fa99d4999cbad882bb35
Kalle Ahlström <71292737+kahlstrm@users.noreply.github.com>
2024-06-21 11:40:22 +0300

    Revert "chore: bump typescript to 5.5.2 (#395)" (#396)
    This reverts commit a316558a049b229044ffe087111754ce65a98981.

--- apps/cms/src/payload.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "@tietokilta/cms-types/payload";
-
-declare module "payload" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface -- not applicable
-  export interface GeneratedTypes extends Config {}
-}
