02f1c23db44a997623a00eaca843478d39de64e6
Mikael Siidorow <mikael@siidorow.com>
2024-07-27 19:00:04 +0300

    feat: add input and textarea components

---
+++ packages/ui/lib/components/textarea/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { cn } from "../../utils";
+
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border-2 border-gray-900 bg-gray-100 px-3 py-2 text-sm ring-offset-gray-800 placeholder:text-gray-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
