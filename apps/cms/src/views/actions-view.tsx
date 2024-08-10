c172edb20840c0c85518ce931232bb84c2cc4ed0
Joonatan Korpela <joonatan.korpela@aalto.fi>
2024-08-10 20:44:04 +0300

    feat: Link committee photos

--- apps/cms/src/views/actions-view.tsx
+++ apps/cms/src/views/actions-view.tsx
@@ -16,7 +16,7 @@ export const ActionsView = (): React.JSX.Element => {
       body: JSON.stringify({ csv, year }),
     });
 
-  const handleSubmit = async (
+  const handleSubmitImportCommittees = async (
     e: React.FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     e.preventDefault();
@@ -45,9 +45,58 @@ export const ActionsView = (): React.JSX.Element => {
     }
   };
 
+  const handleSubmitLinkCommitteeImages = async (
+    e: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const formData = new FormData(e.currentTarget);
+
+    const year = formData.get("year");
+
+    if (!year) {
+      alert("Invalid year");
+      return;
+    }
+
+    const result = await fetch("/api/committee-members/link-photos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ year }),
+    });
+    if (result.ok) {
+      setResultMessage("Linking successful!");
+    } else {
+      setResultMessage(`Linking failed. ${await result.text()}`);
+    }
+  };
+
   return (
-    <div style={{ margin: 20 }}>
+    <div
+      style={{
+        margin: 20,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "start",
+        gap: 30,
+      }}
+    >
       <a href="/admin">Back</a>
+      <span
+        style={{
+          color: "red",
+          fontSize: "2em",
+          WebkitTextStroke: "1px white",
+          border: "2px solid red",
+          padding: "10px",
+        }}
+      >
+        ⚠️ DO NOT USE UNLESS YOU KNOW WHAT YOU'RE DOING! ⚠️
+      </span>
+      {resultMessage && <h2 style={{ color: "success" }}>{resultMessage}</h2>}
       <h1>Actions</h1>
       <form
         style={{
@@ -56,7 +105,9 @@ export const ActionsView = (): React.JSX.Element => {
           alignItems: "flex-start",
           gap: 10,
         }}
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => void handleSubmit(e)}
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+          void handleSubmitImportCommittees(e)
+        }
       >
         <h2>Import committees</h2>
         <label htmlFor="year">Committee year</label>
@@ -72,18 +123,32 @@ export const ActionsView = (): React.JSX.Element => {
         </label>
         <input id="file" name="file" type="file" />
         <button type="submit">Upload</button>
-        <span
-          style={{
-            color: "red",
-            fontSize: "2em",
-            WebkitTextStroke: "1px white",
-            border: "2px solid red",
-            padding: "10px",
-          }}
-        >
-          ⚠️ DO NOT USE UNLESS YOU KNOW WHAT YOU'RE DOING! ⚠️
-        </span>
-        {resultMessage && <p style={{ color: "success" }}>{resultMessage}</p>}
+      </form>
+
+      <form
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+          void handleSubmitLinkCommitteeImages(e)
+        }
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          gap: 10,
+        }}
+      >
+        <h2>Link committee images</h2>
+        <p>
+          Link committee images to the committee members based on name in the
+          given year.
+        </p>
+        <input
+          id="year"
+          name="year"
+          type="number"
+          placeholder="Committee year"
+          defaultValue={new Date().getFullYear().toString()}
+        />
+        <button type="submit">Link</button>
       </form>
     </div>
   );
