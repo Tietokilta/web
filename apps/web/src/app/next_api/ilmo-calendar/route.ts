5ea827577d8afc498b028c1aacd3832baff06d2a
Mikael Siidorow <mikael@siidorow.com>
2024-08-14 20:58:02 +0300

    fix(ics): use host for uids, origin for urls

--- apps/web/src/app/next_api/ilmo-calendar/route.ts
+++ apps/web/src/app/next_api/ilmo-calendar/route.ts
@@ -8,13 +8,14 @@ import { createEvents } from "../../../lib/ics";
 export async function GET(request: NextRequest) {
   const events = await fetchEvents();
 
-  const originUrl = request.nextUrl.origin;
+  const host = request.nextUrl.host;
+  const origin = request.nextUrl.origin;
 
   if (!events.ok) {
     return new Response("Failed to fetch events", { status: 500 });
   }
 
-  const icsEvents = createEvents(events.data, { originUrl });
+  const icsEvents = createEvents(events.data, { host, origin });
 
   return new Response(icsEvents, {
     headers: {
