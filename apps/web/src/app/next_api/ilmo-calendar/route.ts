4747dd45185352d64457f6efde878abaae4c616a
Mikael Siidorow <mikael@siidorow.com>
2024-08-14 22:39:05 +0300

    fix(ics): only remove port in production to keep it working locally

--- apps/web/src/app/next_api/ilmo-calendar/route.ts
+++ apps/web/src/app/next_api/ilmo-calendar/route.ts
@@ -13,7 +13,9 @@ export async function GET(request: NextRequest) {
   // See https://github.com/vercel/next.js/issues/37536#issuecomment-1157000990
   request.nextUrl.host = request.headers.get("Host") ?? request.nextUrl.host;
   // Remove the port and trust the browser to use the correct one to connect to Azure
-  request.nextUrl.port = "";
+  if (process.env.NODE_ENV === "production") {
+    request.nextUrl.port = "";
+  }
   const host = request.nextUrl.host;
   const origin = request.nextUrl.origin;
 
