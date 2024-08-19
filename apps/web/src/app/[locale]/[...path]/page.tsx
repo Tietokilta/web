e8b04f76173c0713ea623868c6ff3d0342910280
Mikael Siidorow <mikael@siidorow.com>
2024-08-19 11:52:07 +0300

    fix: add some padding to page headers

--- apps/web/src/app/[locale]/[...path]/page.tsx
+++ apps/web/src/app/[locale]/[...path]/page.tsx
@@ -142,7 +142,7 @@ async function Page({ params: { path } }: Props) {
         id="main"
         className="relative mb-8 flex flex-col items-center gap-2 md:gap-6"
       >
-        <header className="flex h-[15svh] w-full items-center justify-center bg-gray-900 text-gray-100 md:h-[25svh]">
+        <header className="flex h-[15svh] w-full items-center justify-center bg-gray-900 p-2 text-gray-100 md:h-[25svh]">
           <h1 className="font-mono text-4xl md:text-5xl">{page.title}</h1>
         </header>
 
