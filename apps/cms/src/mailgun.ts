15b9a380461a1fcfbe0355a75a3f53830cbabb28
mikko <mikko.juusti@aalto.fi>
2024-09-01 19:32:19 +0300

    Add support for generating and sending weekly newsletter emails.

---
+++ apps/cms/src/mailgun.ts
@@ -0,0 +1,37 @@
+// mailgun.ts
+
+import FormData from "form-data";
+import Mailgun from "mailgun.js";
+
+interface SendEmailOptions {
+  subject: string;
+  html: string;
+}
+
+export const sendEmail = async (options: SendEmailOptions) => {
+  const mailgun = new Mailgun(FormData);
+  const {
+    MAILGUN_SENDER,
+    MAILGUN_RECEIVER,
+    MAILGUN_API_KEY,
+    MAILGUN_DOMAIN,
+    MAILGUN_URL,
+  } = process.env;
+
+  const mg = mailgun.client({
+    username: "api",
+    url: MAILGUN_URL,
+    key: MAILGUN_API_KEY ?? "",
+  });
+
+  if (!MAILGUN_DOMAIN) {
+    throw Error("MAILGUN DOMAIN NOT FOUND");
+  }
+  const result = await mg.messages.create(MAILGUN_DOMAIN, {
+    from: MAILGUN_SENDER,
+    to: MAILGUN_RECEIVER,
+    subject: options.subject,
+    html: options.html,
+  });
+  return result;
+};
