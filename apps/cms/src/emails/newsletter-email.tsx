1c377535c20228fca0a4d87f522fb20c1e4a574c
mikko <mikko.juusti@aalto.fi>
2024-09-09 23:24:16 +0300

    Remove unused tailwind classes from cms, remove unused components from cms lexical serializer. Add authorization for all admin email endpoints.

--- apps/cms/src/emails/newsletter-email.tsx
+++ apps/cms/src/emails/newsletter-email.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { type WeeklyNewsletter } from "@tietokilta/cms-types/payload";
 import { Html } from "@react-email/components";
 import { newsletterPreviewProps } from "./newsletter-example";
-import Newsletter from "./newsletter";
+import { Newsletter } from "./newsletter";
 
 interface NewsletterEmailProps {
   finnishNewsletter: WeeklyNewsletter;
@@ -19,23 +19,18 @@ export const NewsletterEmail = ({
 }: NewsletterEmailProps): React.ReactElement => {
   return (
     <Html>
-      <main
-        id="main"
-        className="relative mb-8 flex flex-col items-center gap-2 md:gap-6"
-      >
-        <Newsletter
-          newsletter={finnishNewsletter}
-          locale="fi"
-          PUBLIC_LEGACY_URL={PUBLIC_LEGACY_URL}
-          PUBLIC_FRONTEND_URL={PUBLIC_FRONTEND_URL}
-        ></Newsletter>
-        <Newsletter
-          newsletter={englishNewsletter}
-          locale="en"
-          PUBLIC_LEGACY_URL={PUBLIC_LEGACY_URL}
-          PUBLIC_FRONTEND_URL={PUBLIC_FRONTEND_URL}
-        ></Newsletter>
-      </main>
+      <Newsletter
+        newsletter={finnishNewsletter}
+        locale="fi"
+        PUBLIC_LEGACY_URL={PUBLIC_LEGACY_URL}
+        PUBLIC_FRONTEND_URL={PUBLIC_FRONTEND_URL}
+      ></Newsletter>
+      <Newsletter
+        newsletter={englishNewsletter}
+        locale="en"
+        PUBLIC_LEGACY_URL={PUBLIC_LEGACY_URL}
+        PUBLIC_FRONTEND_URL={PUBLIC_FRONTEND_URL}
+      ></Newsletter>
     </Html>
   );
 };
