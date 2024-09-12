d41ad5f1850829c8df959069c9e34f33de553de2
TagLin3 <137885191+TagLin3@users.noreply.github.com>
2024-09-12 09:31:53 +0300

    feat: Improved items list in invoice generator (#491)
    * feat:
    
    Improved items list in invoice generator
    
    * fix:
    
    added localization to item titles
    
    * fix: changed a <span> to a <fieldset>
    
    * fix: removed (mistyped) mt-6 class from item component

--- apps/web/src/components/invoice-generator/index.tsx
+++ apps/web/src/components/invoice-generator/index.tsx
@@ -129,10 +129,21 @@ function SubmitButton({
   );
 }
 
-function DeleteButton({ onClick }: { onClick: () => void }) {
+function DeleteButton({
+  onClick,
+  disabled,
+}: {
+  onClick: () => void;
+  disabled: boolean;
+}) {
   const t = useScopedI18n("invoicegenerator");
   return (
-    <Button className="my-8" onClick={onClick} type="button">
+    <Button
+      className="my-8"
+      onClick={onClick}
+      type="button"
+      disabled={disabled}
+    >
       {t("Remove")}
     </Button>
   );
@@ -175,20 +186,17 @@ function InputRowArray({
       <fieldset id={htmlId} name={name}>
         <div>
           {rows.map((row, index) => (
-            <div
-              className="mt-6"
-              key={row}
-              id={`${htmlId}.${index.toString()}`}
-            >
+            <div key={row} id={`${htmlId}.${index.toString()}`}>
+              <h3>
+                {t("Product")} {index + 1}
+              </h3>
               <Row state={state} index={index} />
-              {/* Do not add delete button for first row because the invoice has to always have at least one row */}
-              {minimumRows !== 1 || index > 0 ? (
-                <DeleteButton
-                  onClick={() => {
-                    setRows(rows.filter((filterRow) => filterRow !== row));
-                  }}
-                />
-              ) : null}
+              <DeleteButton
+                disabled={rows.length === minimumRows}
+                onClick={() => {
+                  setRows(rows.filter((filterRow) => filterRow !== row));
+                }}
+              />
             </div>
           ))}
         </div>
@@ -244,31 +252,37 @@ function InvoiceItem({
           required
         />
       </ErrorMessageBlock>
-      <ErrorMessageBlock
-        elementName={`rows[${index.toString()}].quantity`}
-        formState={state}
-      >
-        <InputRow
-          label={t("Quantity")}
-          name="rows.quantity"
-          id={`rows[${index.toString()}].quantity`}
-          type="number"
-          required
-        />
-      </ErrorMessageBlock>
-      <ErrorMessageBlock
-        elementName={`rows[${index.toString()}].unit`}
-        formState={state}
-      >
-        <InputRow
-          label={t("Unit")}
-          name="rows.unit"
-          id={`rows[${index.toString()}].unit`}
-          defaultValue="kpl"
-          maxLength={128}
-          required
-        />
-      </ErrorMessageBlock>
+      <fieldset className="flex">
+        <span className="mr-0.5 grow">
+          <ErrorMessageBlock
+            elementName={`rows[${index.toString()}].quantity`}
+            formState={state}
+          >
+            <InputRow
+              label={t("Quantity")}
+              name="rows.quantity"
+              id={`rows[${index.toString()}].quantity`}
+              type="number"
+              required
+            />
+          </ErrorMessageBlock>
+        </span>
+        <span className="ml-0.5 grow">
+          <ErrorMessageBlock
+            elementName={`rows[${index.toString()}].unit`}
+            formState={state}
+          >
+            <InputRow
+              label={t("Unit")}
+              name="rows.unit"
+              id={`rows[${index.toString()}].unit`}
+              defaultValue="kpl"
+              maxLength={128}
+              required
+            />
+          </ErrorMessageBlock>
+        </span>
+      </fieldset>
       <ErrorMessageBlock
         elementName={`rows[${index.toString()}].unit_price`}
         formState={state}
