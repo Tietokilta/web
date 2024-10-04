5a4ee864dbdbed4bb6fc4829b992b14ebb7b95f8
Emilia Repo <emiliarepo@icloud.com>
2024-10-04 11:22:55 +0300

    Feat: Badges of honor (#515)
    Add support for badges of honor

---
+++ apps/cms/src/collections/honors/honors.ts
@@ -0,0 +1,38 @@
+import type { CollectionConfig } from "payload/types";
+import { signedIn } from "../../access/signed-in";
+
+export const Honors = {
+  slug: "honors",
+  defaultSort: "-name",
+  admin: {
+    useAsTitle: "name",
+    defaultColumns: ["name", "awardedHonors"],
+  },
+  access: {
+    read: () => true,
+    create: signedIn,
+    update: signedIn,
+    delete: signedIn,
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+      localized: true,
+    },
+    {
+      name: "awardedHonors",
+      type: "array",
+      required: true,
+      minRows: 1,
+      fields: [
+        {
+          name: "awardedHonor",
+          type: "relationship",
+          relationTo: "awarded-honors",
+        },
+      ],
+    },
+  ],
+} as const satisfies CollectionConfig;
