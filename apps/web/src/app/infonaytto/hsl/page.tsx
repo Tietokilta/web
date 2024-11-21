2a533195e035543e31fbbc216d7ea0929995db38
lanttu1243 <lasse.kukkula@gmail.com>
2024-11-21 20:39:07 +0200

    remove hsl page

--- apps/web/src/app/infonaytto/hsl/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import {HSLcombinedSchedule} from "../../../components/infoscreen/hsl-schedules-combined";
-import Eventslist from "../../../components/events-list";
-
-export function HSL() {
-  return <HSLcombinedSchedule />;
-}
-export default HSL;
