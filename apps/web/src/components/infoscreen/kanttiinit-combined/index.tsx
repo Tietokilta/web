a11b7d7c5deff7c61298d4d7816ec99b9d635680
IiroP <iiro.poutiainen@gmail.com>
2024-11-22 18:49:59 +0200

    feat: Added basic styling to Kanttiinit on infoscreen

--- apps/web/src/components/infoscreen/kanttiinit-combined/index.tsx
+++ apps/web/src/components/infoscreen/kanttiinit-combined/index.tsx
@@ -14,14 +14,19 @@ export function KanttiinitCombined({ menus }: { menus: RestaurantMenu[] }) {
       </div>
     );
   }*/
+  const className = `shadow-solid shadow-black text-l rounded-md border-2 border-black p-3 font-mono text-gray-900 md:items-center`;
+
   return (
-    <div>
+    <div className="flex w-full justify-center">
       {menus.map((menu) => (
-        <div key={menu.restaurant.id}>
-          <p>
-            {menu.restaurant.name} ({menu.restaurant.id})
-          </p>
-          <ul>
+        <div
+          key={menu.restaurant.id}
+          className="flex w-full flex-col gap-4 p-2 pt-0"
+        >
+          <h1 className="flex h-12 justify-center p-2 font-mono text-2xl font-bold">
+            {menu.restaurant.name}
+          </h1>
+          <ul className={className}>
             {menu.menus.map((dayMenu) =>
               dayMenu.foods.map((food: Food, index: number) => (
                 <li key={index}>{food.title}</li>
