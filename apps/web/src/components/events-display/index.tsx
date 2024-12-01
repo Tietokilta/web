be1b884a904dba13dc7213763c0b6902e07dab70
Joonatan Korpela <joonatan.korpela@aalto.fi>
2024-12-01 20:44:46 +0200

    chore: Remove SySe stuff

--- apps/web/src/components/events-display/index.tsx
+++ apps/web/src/components/events-display/index.tsx
@@ -182,10 +182,7 @@ export async function EventsDisplay({
         className="font-mono text-2xl font-bold text-gray-900 underline-offset-2 hover:underline"
         href={eventsListPath ?? `/${locale}/${t("ilmomasiina.path.events")}`}
       >
-        <h2
-          className="glitch layers font-mono text-2xl font-bold text-gray-900"
-          data-text={t("heading.Upcoming events")}
-        >
+        <h2 className="font-mono text-2xl font-bold text-gray-900">
           {t("heading.Upcoming events")}
         </h2>
       </Link>
--- apps/web/src/components/hero/index.tsx
+++ apps/web/src/components/hero/index.tsx
@@ -74,17 +74,6 @@ export function Hero({
     new Date().getUTCDate() % images.length,
   );
 
-  // eslint-disable-next-line no-param-reassign -- Syse code go brr
-  texts = [
-    "The destruction is near",
-    "Join System Seven today!",
-    "Maximize your potential",
-    "Productivity is the key to success",
-    "Stop the slack",
-    "Work hard, play hard",
-    "Down with the guild!",
-  ];
-
   const [currentText, setCurrentText] = useState(texts[0]);
   // eslint-disable-next-line react/hook-use-state -- prev index used directly in the setter
   const [, setTextIndex] = useState(0);
