51b69ec3283f177894a2510d359ab10b6905ccce
Luukas Pörtfors <lajp@iki.fi>
2025-01-05 23:51:15 +0200

    feat: add nix flake and .devcontainer.json

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+      devenv-test = self.devShells.${system}.default.config.test;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [bash mongodb-tools docker];
+
+              env = {
+                LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/";
+                PATH = "/usr/local/bin";
+              };
+
+              languages.javascript = {
+                enable = true;
+                package = pkgs.nodejs-slim_22;
+                pnpm.enable = true;
+                pnpm.install.enable = true;
+                bun.enable = true;
+                bun.install.enable = true;
+              };
+            }
+          ];
+        };
+      });
+  };
+}
