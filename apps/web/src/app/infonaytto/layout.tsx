f83751b892f5c33b503502a677184afcf218b8fe
IiroP <iiro.poutiainen@gmail.com>
2025-01-10 17:24:43 +0200

    Revert infoscreen layout/page structure change
    - It seems that it is mandatory to have both layout and page files

---
+++ apps/web/src/app/infonaytto/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+// eslint-disable-next-line camelcase -- Roboto_Mono name is set by next/font
+import { Inter, Roboto_Mono } from "next/font/google";
+import { cn } from "../../lib/utils.ts";
+import "../globals.css";
+import { InfoScreenHeader } from "../../components/infoscreen/infoscreen-header";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+});
+
+export default function ScreenLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fi">
+      <body
+        className={cn(
+          inter.variable,
+          robotoMono.variable,
+          "flex h-full flex-col bg-gray-200 font-mono",
+        )}
+      >
+        <InfoScreenHeader />
+        <div className="size-full p-4">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/src/app/infonaytto/naytto/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { InfoScreenContents } from "../../../components/infoscreen/info-screen-contents";
+
+function InfoScreen() {
+  return <InfoScreenContents />;
+}
+
+export default InfoScreen;
