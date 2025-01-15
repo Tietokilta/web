fa9602ce2b91cc9d697d4a46307abf0420c970fb
Luukas Pörtfors <lajp@iki.fi>
2025-01-15 16:07:34 +0200

    feat: add nix flake and .devcontainer.json (#594)
    Co-authored-by: Kalle Ahlström <kalle.ahlstrom@iki.fi>
    Co-authored-by: Mikael Siidorow <mikael@siidorow.com>

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+      devenv-test = self.devShells.${system}.default.config.test;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        bun = pkgs.bun.overrideAttrs (oldAttrs: {
+          buildInputs = oldAttrs.buildInputs or [] ++ [pkgs.makeWrapper];
+          postInstall =
+            oldAttrs.postInstall
+            or ""
+            + ''
+              wrapProgram $out/bin/bun \
+                --set LD_LIBRARY_PATH "${pkgs.stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH"
+            '';
+        });
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [bash mongodb-tools docker bun];
+
+              # FIXME: this is only required within the dev container
+              enterShell = ''
+                export PATH="$PATH:/usr/local/bin"
+              '';
+
+              languages.javascript = {
+                enable = true;
+                package = pkgs.nodejs-slim_22;
+                pnpm.enable = true;
+                pnpm.install.enable = true;
+              };
+            }
+          ];
+        };
+      });
+  };
+}
