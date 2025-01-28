4837a686e3de6f4960a6ba215685d4b037eda196
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-01-29 01:40:16 +0200

    feat(infoscreen): add correct page metadata to infoscreen

--- apps/web/src/app/[locale]/(infoscreen)/infoscreen/layout.tsx
+++ apps/web/src/app/[locale]/(infoscreen)/infoscreen/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 // eslint-disable-next-line camelcase -- Roboto_Mono name is set by next/font
 import { Inter, Roboto_Mono } from "next/font/google";
+import { type Metadata } from "next";
 import { cn } from "@lib/utils.ts";
 import "../../globals.css";
 import { InfoScreenHeader } from "@components/infoscreen/infoscreen-header/index.tsx";
+import { type LayoutProps } from "../../(main)/layout";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,13 +16,60 @@ const robotoMono = Roboto_Mono({
   variable: "--font-roboto-mono",
 });
 
-export default function ScreenLayout({
+const localizedMetadata = {
+  fi: {
+    title: "Infonäyttö - Tietokilta",
+    description: "Tietokilta ry:n kotisivut",
+  },
+  en: {
+    title: "Infoscreen - Computer Science Guild",
+    description: "Homepage of the Computer Science Guild",
+  },
+} as const;
+
+const mainUrl = process.env.PUBLIC_FRONTEND_URL ?? "https://tietokilta.fi";
+
+const icons = {
+  icon: [
+    {
+      rel: "icon",
+      type: "image/png",
+      media: "(prefers-color-scheme: light)",
+      url: "/icon_dark.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      media: "(prefers-color-scheme: dark)",
+      url: "/icon_light.png",
+    },
+  ],
+};
+
+export const generateMetadata = async (
+  props: LayoutProps,
+): Promise<Metadata> => {
+  const { locale } = await props.params;
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- extra safety
+    ...(localizedMetadata[locale] || localizedMetadata.fi),
+
+    metadataBase: new URL(mainUrl),
+    generator: "Next.js",
+    creator: "Tietokilta ry",
+    icons,
+  };
+};
+
+export default async function ScreenLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
-}) {
+} & LayoutProps) {
+  const { locale } = await params;
   return (
-    <html lang="fi">
+    <html lang={locale}>
       <body
         className={cn(
           inter.variable,
