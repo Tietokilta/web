af0387bcbd7d18b9362e1add72eff66139ad40e7
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-02-04 12:19:51 +0200

    fix: change uploads Link to plain a-tag

--- apps/web/src/components/lexical/lexical-serializer.tsx
+++ apps/web/src/components/lexical/lexical-serializer.tsx
@@ -227,12 +227,12 @@ export function LexicalSerializer({ nodes }: { nodes: Node[] }): JSX.Element {
             const thumbnail = node.value.thumbnail as Media | undefined;
 
             return (
-              <Link
+              <a
                 href={node.value.url ?? "#broken-url"}
                 key={index}
-                prefetch={false}
                 target="_blank"
                 className="not-prose shadow-solid my-4 flex w-fit max-w-full items-center gap-4 text-clip rounded-md border-2 border-gray-900 p-4 hover:border-gray-800 hover:bg-gray-300/90"
+                rel="noopener"
               >
                 <div className="flex max-w-full flex-col items-center gap-2">
                   {thumbnail ? (
@@ -253,7 +253,7 @@ export function LexicalSerializer({ nodes }: { nodes: Node[] }): JSX.Element {
                     </p>
                   ) : null}
                 </div>
-              </Link>
+              </a>
             );
           }
           case "relationship": {
