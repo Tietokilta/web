812a275a3e8827c9f80fddbf5339f1f3668cf808
Emilia Repo <emiliarepo@icloud.com>
2025-02-09 00:48:17 +0200

    add useful comment

--- apps/web/src/lib/api/committees.ts
+++ apps/web/src/lib/api/committees.ts
@@ -2,6 +2,7 @@ import type { Committee } from "@tietokilta/cms-types/payload";
 import { getAllCollectionItems } from "./fetcher";
 
 export const fetchCommittees = getAllCollectionItems<
+  // using not_equals filter for backwards compatibility with old committees
   { where: { year: { equals: string }; hidden: { not_equals: boolean } } },
   Committee[]
 >("committees");
