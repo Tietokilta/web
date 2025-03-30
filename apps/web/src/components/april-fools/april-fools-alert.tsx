ca99a7cd03d02f508c9bf5647cae978a95a9675d
Joonatan Korpela <joonatan.korpela@aalto.fi>
2025-03-30 18:17:38 +0300

    feat: Add localization to april fools joke

--- apps/web/src/components/april-fools/april-fools-alert.tsx
+++ apps/web/src/components/april-fools/april-fools-alert.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { useEffect } from "react";
+import { useCurrentLocale } from "@locales/client";
 
 export default function AprilFoolsAlert() {
+  const locale = useCurrentLocale();
   useEffect(() => {
     const isAprilFoolsDay =
       new Date().getDate() === 1 && new Date().getMonth() === 3;
@@ -11,7 +13,9 @@ export default function AprilFoolsAlert() {
       if (
         // eslint-disable-next-line no-alert -- funny message
         confirm(
-          "Onneksi olkoon, olet juuri voittanut upouuden IPhone 17 Pro Maxin! Lunastaaksesi voiton, klikkaa OK ja syötä puhelinnumerosi seuraavassa ikkunassa. Kiitos osallistumisesta!",
+          locale === "fi"
+            ? "Onneksi olkoon, olet juuri voittanut upouuden IPhone 17 Pro Maxin! Lunastaaksesi voiton, klikkaa OK ja syötä puhelinnumerosi seuraavassa ikkunassa. Kiitos osallistumisesta!"
+            : "Congratulations, you've just won a brand new iPhone 17 Pro Max! To claim your prize, click OK and enter your phone number in the next window. Thanks for participating!",
         )
       ) {
         window.location.href = "https://www.jayna.fi/fi";
