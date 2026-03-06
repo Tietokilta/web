dd59dd2c8597c54ea7740b41dd319182e133084c
lanttu1243 <lasse.kukkula@gmail.com>
2026-03-06 16:24:45 +0200

    Fix typo

--- apps/web/src/components/infoscreen/infoscreen-header/index.tsx
+++ apps/web/src/components/infoscreen/infoscreen-header/index.tsx
@@ -6,7 +6,7 @@ import { InfoscreenClock } from "./clock";
 export function InfoScreenHeader() {
   return (
     <div className="flex h-32 justify-between bg-black p-4 text-white">
-      <div className="flex h-20 space-y-2">
+      <div className="flex h-24 space-y-2">
         <Image
           alt="Tietokilta"
           className="size-24 p-2"
